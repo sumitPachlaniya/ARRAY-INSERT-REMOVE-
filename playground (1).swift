3a3e7a9fef47b4ca5b8c936fd2781301b4b89b48
Sumit Pachlaniya <97600179+sumitPachlaniya@users.noreply.github.com>
2022-02-15 09:59:10 -0800

    Add files via upload

---
+++ playground (1).swift
@@ -0,0 +1,9 @@
+var names = ["Sumit","Sandeep","Vinay","Harsh"]
+names.insert("Ranjit" ,at:2)
+print("Updated Names: \(names)")
+names.removeLast()
+print(names)
+names.remove(at:2)
+print(names)
+names.removeAll()
+print(names)
